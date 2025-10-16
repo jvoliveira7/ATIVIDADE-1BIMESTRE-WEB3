b40e0db6cc7f377e1db2792e25e35a55b6213e89
jvoliveira7 <oliveirajoaovictor774@gmail.com>
2025-10-16 00:05:25 -0300

    feat: Adicionado estrutura de Roles e associação com Users

--- meu_app/db/schema.rb
+++ meu_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_15_060527) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_16_020342) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -41,6 +41,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_15_060527) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -51,9 +57,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_15_060527) do
     t.string "suap_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "role_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "users", "roles"
 end
